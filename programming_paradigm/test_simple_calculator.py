d78ac79e33a4df66f2ead826dfc7d2ae8317c364
Mubarek Adem <mubarekademy@gmail.com>
2025-07-28 09:32:09 +0300

    added programming paradigm

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,30 @@
+# File: programming_paradigm/test_simple_calculator.py
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(3, 4), 7)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 3), -3)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 5), 10)
+        self.assertEqual(self.calc.multiply(-2, 4), -8)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertIsNone(self.calc.divide(10, 0))
+
+
+if __name__ == '__main__':
+    unittest.main()
